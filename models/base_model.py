0ae3d1fbf5cf329fd6744d526bd59e3f335db982
SeckMohameth <mohamethseck34@gmail.com>
2019-06-28 23:44:59 +0000

    problems 3 & 4

--- models/base_model.py
+++ models/base_model.py
@@ -13,10 +13,29 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """ Public instance attributes initialize """
+
+        if kwargs is not None:
+            for key in kwargs:
+                if key == 'id':
+                    self.id = kwargs[key]
+                elif key == 'created_at':
+                    #print(type(kwargs.get('created_at')))
+                    y = datetime.strftime(kwargs.get('created_at'),
+                                          "%Y-%m-%dT%H:%M:%S.%f")
+                    #print(type(y))
+                    #self.created_at = str(y)
+                elif key == 'updated_at':
+                    x = datetime.strftime(kwargs.get('updated_at'),
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                    #print(type(x))
+                    #self.updated_at = str(x)
+                else:
+                    if key != '__class__':
+                        setattr(self, key, kwargs[key])
+
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
@@ -30,15 +49,12 @@ class BaseModel:
         """ Returns a dictionary w/ Keys/vals of __dict__ instance
         Return: dictionary w/ all keys/values
         """
-        a = dict(self.__dict__)
-
-        for key in a:
+        self.__dict__.update({'__class__' : '{}'.format(self.__class__.__name__)})
+        for key in self.__dict__:
             if key == 'id':
-                a[key] = self.id
+                self.__dict__[key] = self.id
             elif key == 'created_at':
-                a[key] = self.created_at.isoformat()
+                self.__dict__[key] = self.created_at.isoformat()
             elif key == 'updated_at':
-                a[key] = self.updated_at.isoformat()
-            elif key == '__class__':
-                a[key] = self.updated_at.isoformat()
-            return a
+                self.__dict__[key] = self.updated_at.isoformat()
+            return self.__dict__
