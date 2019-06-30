aca1ac3d8e9c48fead4957d4f5c040c66427f2a6
SeckMohameth <mohamethseck34@gmail.com>
2019-06-30 01:13:56 +0000

    fixed ISO form issue

--- models/base_model.py
+++ models/base_model.py
@@ -5,8 +5,9 @@ BaseModel Class of Models Module
 
 from uuid import uuid4
 from datetime import datetime
+import json
+import models
 
-time_now = datetime.now
 
 class BaseModel:
     """ class BaseModel"""
@@ -14,42 +15,38 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """ Public instance attributes initialize """
 
-        if kwargs is not None:
+        if kwargs:
             for key in kwargs:
                 if key == 'id':
-                    self.id = kwargs[key]
-                elif key == 'created_at':
-                    #print(type(kwargs.get('created_at')))
-                    y = datetime.strftime(kwargs.get('created_at'),
-                                          "%Y-%m-%dT%H:%M:%S.%f")
-                    #print(type(y))
-                    #self.created_at = str(y)
-                elif key == 'updated_at':
-                    x = datetime.strftime(kwargs.get('updated_at'),
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-                    #print(type(x))
-                    #self.updated_at = str(x)
-                else:
-                    if key != '__class__':
-                        setattr(self, key, kwargs[key])
+                    self.id = kwargs['id']
+            if key == 'created_at':
+                self.created_at = datetime.strptime(kwargs['created_at'],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
+            if key == 'updated_at':
+                self.updated_at = datetime.strptime(kwargs['updated_at'],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                if key != '__class__':
+                    setattr(self, key, kwargs[key])
 
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         """ update instance attribute with current datetime """
         self.updated_at = datetime.now()
 
-
     def to_dict(self):
         """ Returns a dictionary w/ Keys/vals of __dict__ instance
         Return: dictionary w/ all keys/values
         """
-        self.__dict__.update({'__class__' : '{}'.format(self.__class__.__name__)})
+        self.__dict__.update({'__class__': '{}'.format
+                              (self.__class__.__name__)})
         for key in self.__dict__:
             if key == 'id':
                 self.__dict__[key] = self.id
